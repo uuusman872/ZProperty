9b15fc9a010545a68ec96f426f57ead945c87242
Usman <uuusman872@gmail.com>
2021-10-05 10:38:36 +0500

    Added Of_plan model

--- offsure/admin.py
+++ offsure/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import OfPlaneInvesments, OffPlanGallery
 # Register your models here.
+
+class GalleryInline(admin.TabularInline):
+    model = OffPlanGallery
+
+
+class OfPlaneInvesmentInline(admin.ModelAdmin):
+    inlines = [GalleryInline]
+
+admin.site.register(OfPlaneInvesments, OfPlaneInvesmentInline)
--- offsure/models.py
+++ offsure/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class OfPlaneInvesments(models.Model):
+    planeName = models.CharField(max_length=200)
+    overview = models.TextField()
+    location = models.CharField(max_length=200)
+    price = models.CharField(max_length=100)
+    complition_date = models.DateField()
+    dahboard_images = models.ImageField(upload_to="Media/OfPlanInvesments/")
+
+    def __str__(self):
+        return self.planeName
+
+class OffPlanGallery(models.Model):
+    plan = models.ForeignKey(OfPlaneInvesments, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="Media/OfPlanInvesments/Gallery")
+    def __str__(self):
+        return self.plan
