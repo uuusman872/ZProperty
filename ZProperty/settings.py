e605fb86d27b0f0fdb71b7f59c54526b1e60a382
Muhammad Usman <uuusman872@gmail.com>
2021-11-30 16:57:29 +0500

    AddedStaticRootFile
    AddedStatic Root files

--- ZProperty/settings.py
+++ ZProperty/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR, "static",
